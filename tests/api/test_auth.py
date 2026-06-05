2d4c43c877117d9a00f3e317801fc3c0e61f1d2a
Александр <sasha.droz299@gmail.com>
2026-06-05 15:41:37 +0300

    Переписаны тесты с новыми фикстурами, реализованы тесты с проверкой ролевой модели

--- tests/api/test_auth.py
+++ tests/api/test_auth.py
@@ -4,11 +4,11 @@ from conftest import api_manager
 from api.api_manager import ApiManager
 
 class TestAuthAPI:
-    def test_register_user(self, api_manager: ApiManager, test_user):
+    def test_register_user(self, super_admin, test_user):
         """
         Тест на регистрацию пользователя.
         """
-        response = api_manager.auth_api.register_user(test_user)
+        response = super_admin.api.auth_api.register_user(test_user)
         response_data = response.json()
 
         # Проверки
@@ -17,7 +17,7 @@ class TestAuthAPI:
         assert "roles" in response_data, "Роли пользователя отсутствуют в ответе"
         assert "USER" in response_data["roles"], "Роль USER должна быть у пользователя"
 
-    def test_register_and_login_user(self, api_manager: ApiManager, registered_user):
+    def test_register_and_login_user(self, common_user, registered_user):
         """
         Тест на регистрацию и авторизацию пользователя.
         """
@@ -25,27 +25,21 @@ class TestAuthAPI:
             "email": registered_user["email"],
             "password": registered_user["password"]
         }
-        response = api_manager.auth_api.login_user(login_data,)
+        response = common_user.api.auth_api.login_user(login_data,)
         response_data = response.json()
 
         # Проверки
         assert "accessToken" in response_data, "Токен доступа отсутствует в ответе"
         assert response_data["user"]["email"] == registered_user["email"], "Email не совпадает"
 
-    def test_get_user_info(self, api_manager: ApiManager, test_user):
-        register_response = api_manager.auth_api.register_user(test_user)
+    def test_get_user_info(self, super_admin, test_user):
+        register_response = super_admin.api.auth_api.register_user(test_user)
         user_id = register_response.json()["id"]
 
-        admin_creds = ('api1@gmail.com', 'asdqwe123Q')
-        api_manager.auth_api.authenticate(admin_creds)  # авторизация
+        response = super_admin.api.user_api.get_user_info(user_id)
 
-        response = api_manager.user_api.get_user_info(user_id)
-
-    def test_delete_user(self, api_manager: ApiManager, test_user):
-        register_response = api_manager.auth_api.register_user(test_user)
+    def test_delete_user(self, super_admin, test_user):
+        register_response = super_admin.api.auth_api.register_user(test_user)
         user_id = register_response.json()["id"]
 
-        admin_creds = ('api1@gmail.com', 'asdqwe123Q')
-        api_manager.auth_api.authenticate(admin_creds) #авторизация
-
-        response = api_manager.user_api.delete_user(user_id)
+        response = super_admin.api.user_api.delete_user(user_id)
